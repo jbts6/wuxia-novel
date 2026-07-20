d765b225cf736fd4891cf90ff59d9182545a81a4
jbts6 <fh345392977@gmail.com>
2026-07-20 12:26:52 +0800

    migrate(game-kb): upgrade 神雕侠侣 to V6

--- 金庸/神雕侠侣/data/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as characters } from './characters.json';
-export { default as skills } from './skills.json';
-export { default as items } from './items.json';
-export { default as factions } from './factions.json';
-export { default as locations } from './locations.json';
-export { default as dialogues } from './dialogues.json';
-export { default as techniques } from './techniques.json';
-export { default as chapter_summaries } from './chapter_summaries.json';
